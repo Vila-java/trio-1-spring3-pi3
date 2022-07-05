1d3146d1df6de258d19b18fea1e35e326fab8a3c
everodrigues <evelin.rodrigues@mercadolivre.com>
2022-07-05 16:37:37 -0300

    initializing spring project

---
+++ concessionariat1/src/main/java/com/concessionariat1/concessionariat1/Concessionariat1Application.java
@@ -0,0 +1,13 @@
+package com.concessionariat1.concessionariat1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Concessionariat1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Concessionariat1Application.class, args);
+    }
+
+}
---
+++ concessionariat1/src/test/java/com/concessionariat1/concessionariat1/Concessionariat1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concessionariat1.concessionariat1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Concessionariat1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
