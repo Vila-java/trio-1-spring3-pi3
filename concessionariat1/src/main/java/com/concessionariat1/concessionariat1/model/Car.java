12d98934df3dcf41c0936c146a09d50642c248e1
BiancaPolegatti <bianca.polegatti@mercadolivre.com>
2022-07-05 18:00:33 -0300

    upload model/service and model/car

---
+++ concessionariat1/src/main/java/com/concessionariat1/concessionariat1/model/Car.java
@@ -0,0 +1,17 @@
+package com.concessionariat1.concessionariat1.model;
+
+import java.util.List;
+
+public class Car {
+     private String brand;
+     private String model;
+     private String manufacturingDate;
+     private double numberOfKilometers;
+     private int doors;
+     private double price;
+     private String currency;
+     private List<Service> listService;
+     private int countOfOwners;
+
+
+}
---
+++ concessionariat1/src/main/java/com/concessionariat1/concessionariat1/model/Service.java
@@ -0,0 +1,9 @@
+package com.concessionariat1.concessionariat1.model;
+
+public class Service {
+
+    private String date;
+    private double kilometers;
+    private String descripitons;
+
+}
